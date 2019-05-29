a484b70c17c980a369b4b359316193e4ba1c4217
pkysetti <pkysetti@gmail.com>
2019-05-28 23:50:59 -0500

    Initial

---
+++ blueprism/src/main/java/com/roothelix/blueprism/BlueprismApplication.java
@@ -0,0 +1,13 @@
+package com.roothelix.blueprism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueprismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueprismApplication.class, args);
+	}
+
+}
---
+++ blueprism/src/test/java/com/roothelix/blueprism/BlueprismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roothelix.blueprism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueprismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
